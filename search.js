f04b0afb25ac80ab3f39727474c1a62a4df8eb9e
jaybojaybojaybo <jasun.feddema@gmail.com>
2018-07-14 00:57:14 -0700

    loop the API call while changing the 'pages' parameter in order to bypass the 20 result limit of Twitter

--- search.js
+++ search.js
@@ -4,18 +4,28 @@ var config = require('./config')
 
 var T = new Twit(config);
 
+var pages = 6; //set pages to increase amount of results received by increments of 20
+
 var params = {
-    q: 'portland',
-    count: 10
+    f: 'users',
+    q: 'portland'
 }
 
-T.get('users/search', params, searchedData);
+let multiDataLists = [];
+
+for(let i = 1; i <= pages; i++){
+    params.page = i;
+    T.get('users/search', params, searchedData);
+}
 
 function searchedData(err, data, response) {
     let dataList = [];
     let tweets = data;
     tweets.forEach(function(tweet){
-        dataList.push(tweet.name);
+        // (tweet.url).toString().includes("github") ? 
+            multiDataLists.push(tweet.name);
+            // (console.log(tweet.name));
     });
-    console.log(dataList);
+    console.log(multiDataLists.length);
 }
+
